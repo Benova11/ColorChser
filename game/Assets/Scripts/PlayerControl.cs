615f2608712a54eeb544da0c9df5b215a5d8637c
ben <ben@gamejamtech.com>
2021-02-12 18:02:03 +0200

    scene configx

--- game/Assets/Scripts/PlayerControl.cs
+++ game/Assets/Scripts/PlayerControl.cs
@@ -7,20 +7,30 @@ public class PlayerControl : MonoBehaviour
 
     public float moveSpeed;
     public float jumpForce;
-
+    
     private Rigidbody2D myRigidBody;
+
+    public bool isGrounded;
+    public LayerMask whatIsGround; 
+    
+    private Collider2D myCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
         myRigidBody.velocity = new Vector2(moveSpeed,myRigidBody.velocity.y);
 
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
             myRigidBody.velocity += new Vector2(myRigidBody.velocity.x,jumpForce);
         }
         
